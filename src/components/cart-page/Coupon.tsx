7e468dc9bd58b3422df73d9b4b1c043a2735fa01
Ali Dakoumi <dakoumi31@gmail.com>
2024-08-20 10:14:01 +0100

    Coupon styles fixed

--- src/components/cart-page/Coupon.tsx
+++ src/components/cart-page/Coupon.tsx
@@ -2,13 +2,13 @@ function Coupon() {
   return (
     <form className="h-max flex">
       <input
-        className="w-3/4 border-2 placeholder-[#262626] text-[16px] poppins-regular py-2 px-4"
+        className="w-3/4 border-l-2 border-t-2 border-b-2 placeholder-[#262626] text-[16px] poppins-regular py-2 px-4"
         type="text"
         placeholder="Voucher code"
       />
       <button
         type="submit"
-        className="w-1/4 rounded-sm bg-success text-white py-2 px-4 flex justify-center items-center"
+        className="w-1/4 rounded-tr-sm rounded-br-sm bg-success text-white py-2 px-4 flex justify-center items-center"
       >
         Redeem
       </button>
